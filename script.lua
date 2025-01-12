b2eee212174f6dfd12310a4a133c47ee0eb0fd41
MysticCr1 <142620732+MysticCr1@users.noreply.github.com>
2025-01-12 16:45:29 +1030

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
-local TweenService = game:GetService("TweenService")
 repeat task.wait() until game:IsLoaded()
+local TweenService = game:GetService("TweenService")
 local queue_on_teleport = queue_on_teleport or function(code)
     if syn and syn.queue_on_teleport then
         syn.queue_on_teleport(code)
