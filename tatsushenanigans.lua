731cc5230dbfc6000194cfd088ab8c37b1d88be7
MysticCr1 <142620732+MysticCr1@users.noreply.github.com>
2025-03-23 21:53:05 +1030

    Create tatsushenanigans.lua

---
+++ tatsushenanigans.lua
@@ -0,0 +1,60 @@
+local camera = workspace.CurrentCamera
+local player = game.Players.LocalPlayer
+local char = player.Character
+local hrp = char.HumanoidRootPart
+
+local flip = false
+
+local posTable = {
+    originalPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+}
+function Pausecamera()
+    local previousCFrame = camera.CFrame
+    camera.CameraType = Enum.CameraType.Scriptable
+    camera.CFrame = previousCFrame
+end
+function UnPausecamera()
+    if player.Character and player.Character:FindFirstChild("Humanoid") then
+        camera.CameraType = Enum.CameraType.Custom
+        camera.CameraSubject = player.Character.Humanoid
+    end
+end
+local function subtitle(move, text)
+    game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.Reuse.Text = text
+    game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.Reuse.Reuse.Text = text
+    game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.Reuse.Visible = true   
+end
+
+
+local function onAnimationPlayed(animTrack)
+    if animTrack.Animation.AnimationId == "rbxassetid://16139108718" then
+        posTable.originalPos = hrp.Position
+        Pausecamera()
+        hrp.CFrame = hrp.CFrame * CFrame.new(0, -50, 0)
+        wait(0.3)
+        hrp.CFrame = hrp.CFrame * CFrame.new(0, -490, 0)
+        wait(0.7)
+        UnPausecamera()
+        hrp.CFrame = CFrame.new(posTable.originalPos)
+    end
+end
+game.Players.LocalPlayer.Character.Humanoid.AnimationPlayed:Connect(onAnimationPlayed)
+local function onAnimationPlayed(animTrack)
+    if animTrack.Animation.AnimationId == "rbxassetid://16515850153" then
+        task.spawn(function()
+            flip = true
+            wait(0.5)
+            flip = false
+        end)
+        repeat
+            wait()
+            char.HumanoidRootPart.CFrame = CFrame.new(char.HumanoidRootPart.Position) + Vector3.new(0,-2,0)
+            char.HumanoidRootPart.CFrame = CFrame.new(char.HumanoidRootPart.Position) * CFrame.Angles(math.rad(-90), 0, 0)
+        until not flip
+    end
+end
+game.Players.LocalPlayer.Character.Humanoid.AnimationPlayed:Connect(onAnimationPlayed)
+subtitle("1", "Instakill")
+subtitle("2", "Stun")
+subtitle("3", "Normal")
+subtitle("4", "Normal")
