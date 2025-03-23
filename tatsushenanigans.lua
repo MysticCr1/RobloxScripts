d08b4418c5d7edcfac6f8315ab1b76c03c532338
MysticCr1 <142620732+MysticCr1@users.noreply.github.com>
2025-03-24 00:36:39 +1030

    Update tatsushenanigans.lua

--- tatsushenanigans.lua
+++ tatsushenanigans.lua
@@ -40,11 +40,15 @@ function UnPausecamera()
     end
 end
 
---indicatorbullshit
+--names
+
 local function subtitle(move, text)
     game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.Reuse.Text = text
     game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.Reuse.Reuse.Text = text
     game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.Reuse.Visible = true   
+end
+local function title(move, text)
+    game.Players.LocalPlayer.PlayerGui:FindFirstChild("Hotbar"):FindFirstChild("Backpack"):FindFirstChild("Hotbar"):FindFirstChild(move).Base.ToolName.Text = text
 end
 task.spawn(function()
     local hasdied = false
@@ -55,34 +59,63 @@ task.spawn(function()
         end
         if not isToggled then
             if m1.Text == "Crushing Pull" then
-                subtitle("1", "Instakill")
+                title("1", "Soul Grab")
             end
             if m2.Text == "Windstorm Fury" then
-                subtitle("2", "Stun (Janky)")
+                title("2", "W.I.P")
             end
             if m3.Text == "Stone Coffin" then
-                subtitle("3", "Normal")
+                title("3", "Showstopper")
             end
             if m4.Text == "Expulsive Push" then
+                title("4", "W.I.P")
+            end
+        end
+    until hasdied
+end)
+task.spawn(function()
+    local hasdied = false
+    repeat
+        wait()
+        if humanoid.Health == 0 then
+            hasdied = true
+        end
+        if not isToggled then
+            if m1.Text == "Soul Grab" then
+                subtitle("1", "Instakill")
+            end
+            if m2.Text == "W.I.P" then
+                subtitle("2", "Stun (Janky)")
+            end
+            if m3.Text == "Showstopper" then
+                subtitle("3", "USD (Broken)")
+            end
+            if m4.Text == "W.I.P" then
                 subtitle("4", "Normal")
             end
         end
         if isToggled then
-            if m1.Text == "Crushing Pull" then
+            if m1.Text == "Soul Grab" then
                 subtitle("1", "Jail")
             end
-            if m2.Text == "Windstorm Fury" then
+            if m2.Text == "W.I.P" then
                 subtitle("2", "Stun (Janky)")
             end
-            if m3.Text == "Stone Coffin" then
-                subtitle("3", "Normal")
+            if m3.Text == "Showstopper" then
+                subtitle("3", "USD (Broken)")
             end
-            if m4.Text == "Expulsive Push" then
+            if m4.Text == "W.I.P" then
                 subtitle("4", "Normal")
             end
         end
     until hasdied
 end)
+local ultshit = game.Players.LocalPlayer.PlayerGui:FindFirstChild("ScreenGui"):FindFirstChild("MagicHealth"):FindFirstChild("TextLabel")
+ultshit.Text = "W.I.P"
+ultshit.TextLabel.Text = "W.I.P"
+
+-- moves
+
 local function onAnimationPlayed(animTrack)
     if animTrack.Animation.AnimationId == "rbxassetid://16139108718" then
         if not isToggled then
@@ -99,7 +132,7 @@ local function onAnimationPlayed(animTrack)
             Pausecamera()
             wait(0.3)
             hrp.CFrame = CFrame.new(438.7888488769531, 439.51055908203125, -376.19354248046875)
-            wait(0.7)
+            wait(0.8)
             UnPausecamera()
             hrp.CFrame = CFrame.new(posTable.originalPos)
         end
@@ -121,6 +154,7 @@ local function onAnimationPlayed(animTrack)
     end
 end
 game.Players.LocalPlayer.Character.Humanoid.AnimationPlayed:Connect(onAnimationPlayed)
+
 local function onAnimationPlayed(animTrack)
     if animTrack.Animation.AnimationId == "rbxassetid://16571461202" then
         Pausecamera()
@@ -131,6 +165,26 @@ local function onAnimationPlayed(animTrack)
         hrp.CFrame = CFrame.new(posTable.originalPos)
     end
 end
+game.Players.LocalPlayer.Character.Humanoid.AnimationPlayed:Connect(onAnimationPlayed)
+
+local function onAnimationPlayed(animTrack)
+    if animTrack.Animation.AnimationId == "rbxassetid://16431491215" then
+        task.spawn(function()
+            flip = true
+            wait(0.5)
+            flip = false
+        end)
+        repeat
+            wait()
+            local hrp = character.HumanoidRootPart
+            local currentCFrame = hrp.CFrame
+            local lookDirection = hrp.CFrame.LookVector
+            hrp.CFrame = CFrame.new(currentCFrame.Position) * CFrame.Angles(math.pi, 0, 0)
+            hrp.CFrame = CFrame.lookAt(hrp.Position, hrp.Position + lookDirection)
+        until not flip
+    end
+end
+
 game.Players.LocalPlayer.Character.Humanoid.AnimationPlayed:Connect(onAnimationPlayed)
 
 -- Toggles
